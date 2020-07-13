e3dc19ae591cc10f7df452d5636118e841560a87
dkgiant <dangkynguyhiem@gmail.com>
2020-07-13 09:24:33 +0700

    login using from_response

---
+++ demo_api/spiders/openlibrary_login.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy import FormRequest
+
+class OpenlibraryLoginSpider(scrapy.Spider):
+    name = 'openlibrary_login'
+    allowed_domains = ['openlibrary.org']
+    start_urls = ['https://openlibrary.org/account/login']
+
+    def parse(self, response):
+        yield FormRequest.from_response(
+            response,
+            formid = 'register',
+            formdata ={
+                'username': 'dhgiang85@gmail.com',
+                'password': 'Df7604129',
+                'redirect': '/',
+                'debug_token': '',
+                'login': 'Log In'
+            },
+            callback= self.after_login            
+        )
+    
+    def after_login(self, response):
+        print('log in')
